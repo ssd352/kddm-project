1344c96b6fb0d1523ea549dc32e9d8716df60ac0
ssd352 <27953063+ssd352@users.noreply.github.com>
2017-12-02 20:36:10 -0700

    Add number of classes

--- Python/Project.Sampled.py
+++ Python/Project.Sampled.py
@@ -3,7 +3,7 @@ import hdbscan
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.preprocessing import scale
-from sklearn import metrics # jaccard_similarity_score, silhouette_score
+from sklearn import metrics  # jaccard_similarity_score, silhouette_score
 from sklearn.cluster import KMeans, DBSCAN
 
 if __name__ == "__main__":
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     # HDBSCAN(algorithm='best', alpha=1.0, approx_min_span_tree=True,
     #         gen_min_span_tree=False, leaf_size=40, memory=Memory(cachedir=None),
     #         metric='euclidean', min_cluster_size=5, min_samples=None, p=None)
-      #leaf_size=15)
+        # leaf_size=15)
     NUM_CLASSES = 4199
 
     matrix = np.genfromtxt("../Matrix.csv", delimiter=',')
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     X_scaled = scale(tr, axis=1)
     y_true = np.asarray(list(range(NUM_CLASSES)) * 15)
 
-    SAMPLE = NUM_CLASSES
+    SAMPLE = 5
     picking_ind = [class_num + i * NUM_CLASSES for class_num in range(SAMPLE) for i in range(15)]
     X_sampled = X_scaled[picking_ind, :]
     y_sampled = y_true[picking_ind]
@@ -35,6 +35,7 @@ if __name__ == "__main__":
     clusterer.fit(X_sampled)
     # labels = clusterer.labels_
     print('HDBScan-------------------------------->')
+    print("Number of clusters:", clusterer.labels_.max + 1)
     print("HDBScan Labels:", clusterer.labels_)
     print("Jaccard Index for HDBScan is ", metrics.jaccard_similarity_score(y_sampled, clusterer.labels_))
     print("Rand Index for HDBScan is", metrics.adjusted_rand_score(y_sampled, clusterer.labels_))
@@ -46,6 +47,7 @@ if __name__ == "__main__":
     print('DBScan-------------------------------->')
     db = DBSCAN(eps=1.0, min_samples=5).fit(X_sampled)
     print("DBScan Labels:", db.labels_)
+    print("Number of clusters", db.labels_.max + 1)
     print("Jaccard Index for DBScan is ", metrics.jaccard_similarity_score(y_sampled, db.labels_))
     print("Rand Index for DBScan is", metrics.adjusted_rand_score(y_sampled, db.labels_))
 

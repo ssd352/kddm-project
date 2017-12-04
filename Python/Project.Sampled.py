fea43b005835a74032f81e65f530e2e8ffe77c38
ssd352 <27953063+ssd352@users.noreply.github.com>
2017-12-03 19:17:18 -0700

    Add Silhoutte Score

--- Python/Project.Sampled.py
+++ Python/Project.Sampled.py
@@ -24,35 +24,38 @@ if __name__ == "__main__":
     X_sampled = X_scaled[picking_ind, :]
     y_sampled = y_true[picking_ind]
 
+    print("Silhouette score is", metrics.silhouette_score(X_sampled, y_sampled))
+
     kmeans = KMeans(n_clusters=SAMPLE).fit(X_sampled)
     print('KMeans-------------------------------->')
     print("K-means Labels:", kmeans.labels_)
     np.save("Kmeans.npy", kmeans.labels_)
     print("Jaccard Index for K-means is ", metrics.jaccard_similarity_score(y_sampled, kmeans.labels_))
     print("Rand Index for K-means is", metrics.adjusted_rand_score(y_sampled, kmeans.labels_))
+    print("Silhouette score is", metrics.silhouette_score(X_sampled, kmeans.labels_))
     # print(kmeans.labels_)
 
     clusterer = hdbscan.HDBSCAN()
     clusterer.fit(X_sampled)
     # labels = clusterer.labels_
     print('HDBScan-------------------------------->')
-    print("Number of clusters:", clusterer.labels_.max() + 1)
+    print("Number of clusters is", clusterer.labels_.max() + 1)
     # print("HDBScan Labels:", clusterer.labels_)
     np.save("HDBScan.npy", clusterer.labels_)
     print("Jaccard Index for HDBScan is ", metrics.jaccard_similarity_score(y_sampled, clusterer.labels_))
     print("Rand Index for HDBScan is", metrics.adjusted_rand_score(y_sampled, clusterer.labels_))
-    print("Number of clusters is", clusterer.labels_.max() + 1)
+    print("Silhouette score is", metrics.silhouette_score(X_sampled, clusterer.labels_))
     # clusterer.condensed_tree_.plot(select_clusters=True)
     # plt.show()
     # print("Silhouette score is", metrics.silhouette_score(X_sampled, labels))
 
     print('DBScan-------------------------------->')
-    db = DBSCAN(eps=1.0, min_samples=5).fit(X_sampled)
+    db = DBSCAN(eps=0.1, min_samples=5).fit(X_sampled)
     # print("DBScan Labels:", db.labels_)
     np.save("DBScan.npy", db.labels_)
     print("Number of clusters", db.labels_.max() + 1)
     print("Jaccard Index for DBScan is ", metrics.jaccard_similarity_score(y_sampled, db.labels_))
     print("Rand Index for DBScan is", metrics.adjusted_rand_score(y_sampled, db.labels_))
-
+    print("Silhouette score is", metrics.silhouette_score(X_sampled, db.labels_))
     # clusterer.condensed_tree_.plot(select_clusters=True)
     # clusterer.single_linkage_tree_.plot(cmap='viridis', colorbar=True)

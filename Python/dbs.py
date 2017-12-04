0e2ab593235af12729ba13b305b906be2f863b44
ssd352 <27953063+ssd352@users.noreply.github.com>
2017-12-03 23:05:39 -0700

    Add parameter sweep to DBScan

--- Python/dbs.py
+++ Python/dbs.py
@@ -18,9 +18,12 @@ if __name__ == "__main__":
     y_sampled = y_true[picking_ind]
 
     print('DBScan-------------------------------->')
-    db = DBSCAN(eps=0.05, min_samples=5).fit(X_sampled)
-    np.save("DBScan.npy", db.labels_)
-    print("Number of clusters", db.labels_.max() + 1)
-    print("Jaccard Index for DBScan is ", metrics.jaccard_similarity_score(y_sampled, db.labels_))
-    print("Rand Index for DBScan is", metrics.adjusted_rand_score(y_sampled, db.labels_))
-    print("Silhouette score is", metrics.silhouette_score(X_sampled, db.labels_))
+    for min_samples in range(2, 15 + 1):
+        for eps in [0.01, 0.05, 0.1, 0.125, 0.25, 0.5, 0.75, 1]:
+            print('epsilon is ', eps, 'min points is', min_samples)
+            db = DBSCAN(eps=0.05, min_samples=min_samples).fit(X_sampled)
+            # np.save("DBScan.npy", db.labels_)
+            print("Number of clusters", db.labels_.max() + 1)
+            print("Jaccard Index for DBScan is ", metrics.jaccard_similarity_score(y_sampled, db.labels_))
+            print("Rand Index for DBScan is", metrics.adjusted_rand_score(y_sampled, db.labels_))
+            # print("Silhouette score is", metrics.silhouette_score(X_sampled, db.labels_))
